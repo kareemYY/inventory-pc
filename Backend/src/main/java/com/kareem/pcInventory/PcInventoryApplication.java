6da70afdf918365ffe53a826d5f2597856531f79
Kareem Yasser <kareemyasser264@gmail.com>
2026-07-19 20:55:32 +0300

    Initial project setup

---
+++ Backend/src/main/java/com/kareem/pcInventory/PcInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.kareem.pcInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcInventoryApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/kareem/pcInventory/PcInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kareem.pcInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
